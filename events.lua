5564a2b6416bc6b3a949a4db5ae621ee63dc02f4
Ruben Vincenten <rubenvincenten@gmail.com>
2013-05-25 21:09:45 +0200

    stop event debugging

--- events.lua
+++ events.lua
@@ -4,25 +4,25 @@ local frame = CreateFrame'Frame'
 
 addon.eventFrame = frame
 function addon:RegisterEvent(event, func)
-    self:Debug('Registering ' ..event..' to '.. (func or '<self>:'..event))
+    --self:Debug('Registering ' ..event..' to '.. (func or '<self>:'..event))
     frame:RegisterEvent(event)
     if func then self[event] = func end
     assert(self[event], 'Missing event in table: '..event)
 end
 
 function addon:UnregisterEvent(event)
-    self:Debug('Unregistering ' ..event)
+    --self:Debug('Unregistering ' ..event)
     frame:UnregisterEvent(event)
 end
 
 function addon:UnregisterAllEvents()
-    self:Debug'Unregistering all events'
+    --self:Debug'Unregistering all events'
     frame:UnregisterAllEvents()
 end
 
 do
     local function OnEvent(self, event, ...)
-        addon:Debug('Event: '.. event, ...)
+        --addon:Debug('Event: '.. event, ...)
         if addon[event] then addon[event](addon, ...) end
     end
 
